c765965ffb70cbe6606ec662ccc25adf959bcdb7
jose <josmelendezramos123@gmail.com>
2025-10-22 01:28:56 -0500

    Test: startup.sh configurado para arrancar nginx y colas de laravel

---
+++ deploy/azure/startup.sh
@@ -0,0 +1,7 @@
+cp /home/default /etc/nginx/sites-enabled/default; service nginx restart
+apt-get install -y supervisor
+cp /home/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
+service supervisor start
+supervisorctl reread
+supervisorctl update
+supervisorctl start laravel-worker:*

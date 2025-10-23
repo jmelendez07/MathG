3b91fa7475238ec2e257f6b92c42a095f3c70fae
jose <josemelendezramos123@gmail.com>
2025-10-23 16:18:40 -0500

    Add: librdkafka-dev añadido como libreria de azure

--- deploy/azure/startup.sh
+++ deploy/azure/startup.sh
@@ -1,5 +1,5 @@
 cp /home/default /etc/nginx/sites-enabled/default; service nginx restart
-apt-get install -y supervisor
+apt-get install -y supervisor librdkafka-dev
 cp /home/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
 service supervisor start
 supervisorctl reread

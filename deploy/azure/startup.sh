7d004cd19f1568190b6eb413cb67e4c085f52ced
José Ángel Meléndez Ramos <josemelendezramos123@gmail.com>
2025-10-27 01:11:10 -0500

    Add kafka-consumer configuration to startup script

--- deploy/azure/startup.sh
+++ deploy/azure/startup.sh
@@ -1,7 +1,9 @@
 cp /home/default /etc/nginx/sites-enabled/default; service nginx restart
 apt-get install -y supervisor librdkafka-dev
 cp /home/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
+cp /home/kafka-consumer.conf /etc/supervisor/conf.d/kafka-consumer.conf
 service supervisor start
 supervisorctl reread
 supervisorctl update
 supervisorctl start laravel-worker:*
+supervisorctl start kafka-consumer:*
